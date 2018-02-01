a70beab07058b9f111e28f7f21597ba1554f57c9
Nick Darvey <nickdarvey@outlook.com.au>
2018-02-02 01:29:21 +1100

    Implements singleton service connection

--- ServiceFabric.ConnectionStrings/SingletonServiceConnectionString.cs
+++ ServiceFabric.ConnectionStrings/SingletonServiceConnectionString.cs
@@ -1,13 +1,35 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
 
-//namespace NickDarvey.ServiceFabric.ConnectionStrings
-//{
-//    public class SingletonServiceConnectionString : ServiceConnectionString
-//    {
-//        internal SingletonServiceConnectionString(string connectionString) : base(connectionString)
-//        {
-//        }
-//    }
-//}
+namespace NickDarvey.ServiceFabric.ConnectionStrings
+{
+    public class SingletonServiceConnectionString : ServiceConnectionString
+    {
+        private const string PartitionKindSingleton = "Singleton";
+
+        public SingletonServiceConnectionString(
+            Uri serviceUri) : this(
+            parts: new ServiceConnectionString(serviceUri, PartitionKindSingleton))
+        { }
+
+        internal SingletonServiceConnectionString(
+            ServiceConnectionString parts)
+            : base(parts)
+        { }
+
+        public static new SingletonServiceConnectionString Parse(string connectionString)
+        {
+            var parts = ServiceConnectionString.Parse(connectionString ?? throw new ArgumentNullException(nameof(connectionString)));
+            return new SingletonServiceConnectionString(parts);
+        }
+
+        public static new SingletonServiceConnectionString Parse(IDictionary<string, string> parts)
+        {
+            var serviceConnectionString = ServiceConnectionString.Parse(parts ?? throw new ArgumentNullException(nameof(parts)));
+            return new SingletonServiceConnectionString(serviceConnectionString);
+        }
+
+        public static explicit operator SingletonServiceConnectionString(string s) =>
+            Parse(s);
+    }
+}
